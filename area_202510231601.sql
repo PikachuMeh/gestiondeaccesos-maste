fa88863892c96187de69a359b95872cf1974417e
Juan <103707572+PikachuMeh@users.noreply.github.com>
2025-10-23 16:04:11 -0400

    Subida de bd
    Cambios de la bd y datos de la misma

---
+++ area_202510231601.sql
@@ -0,0 +1,7 @@
+INSERT INTO sistema_gestiones.area (nombre,id_centro_datos) VALUES
+	 ('TELECOMUNICACIONES',1),
+	 ('TELECOMUNICACIONES',2),
+	 ('CROSSCONNECT',2),
+	 ('CPD',1),
+	 ('TELECOMUNICACIONES Y CPD',1),
+	 ('CPD',2);
---
+++ visitas_202510231601.sql
@@ -0,0 +1,13 @@
+INSERT INTO sistema_gestiones.visitas (codigo_visita,persona_id,centro_datos_id,estado_id,tipo_actividad_id,area_id,descripcion_actividad,fecha_programada,fecha_ingreso,fecha_salida,duracion_estimada,autorizado_por,motivo_autorizacion,equipos_ingresados,equipos_retirados,observaciones,notas_finales,activo,fecha_creacion,fecha_actualizacion) VALUES
+	 ('310349272',44,1,1,1,NULL,'DCWADCCW','2025-10-21 11:23:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-20 11:23:29.388046-04',NULL),
+	 ('158210692',44,2,1,1,NULL,'2EE2E2D','2025-10-24 11:30:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-20 11:30:39.374402-04',NULL),
+	 ('017967941',45,1,1,2,NULL,'bgbhbhbh hbhbhbhb','2025-10-21 11:39:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-20 11:39:56.451521-04',NULL),
+	 ('942630333',44,1,1,1,NULL,'jnfnfnfnjfjnnfjkfjnfnkf','2025-10-21 11:45:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-20 11:46:00.788061-04',NULL),
+	 ('297472475',216,1,1,2,NULL,'CPU INTEL TU PAPA','2025-10-21 12:08:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-20 12:09:10.906792-04',NULL),
+	 ('077297038',55,1,1,3,NULL,'dcwadcdaw','2025-10-23 10:51:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-22 10:51:59.041518-04',NULL),
+	 ('329074209',45,1,1,4,NULL,'1213132312312cdeadcawc','2025-10-23 10:58:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-22 10:59:00.714765-04',NULL),
+	 ('044645143',45,1,1,3,6,'dcwacdawcda','2025-10-24 11:39:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-23 11:39:13.584948-04',NULL),
+	 ('585383611',401,1,1,4,1,'cdawdacdadaw','2025-10-24 13:30:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-23 13:30:07.839757-04',NULL),
+	 ('525437608',248,1,1,7,6,'pos no se we, vino a robar ese csm','2025-10-23 21:32:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-23 14:32:57.438358-04',NULL);
+INSERT INTO sistema_gestiones.visitas (codigo_visita,persona_id,centro_datos_id,estado_id,tipo_actividad_id,area_id,descripcion_actividad,fecha_programada,fecha_ingreso,fecha_salida,duracion_estimada,autorizado_por,motivo_autorizacion,equipos_ingresados,equipos_retirados,observaciones,notas_finales,activo,fecha_creacion,fecha_actualizacion) VALUES
+	 ('735258290',401,1,1,1,6,'pues el coso we','2025-10-23 16:48:00-04',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,true,'2025-10-23 14:51:28.809275-04',NULL);
